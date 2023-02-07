51113d1a3ad2a4a432f6b74afb17cd5fdcdb474b
Dmitry <68408042+FonDerMark@users.noreply.github.com>
2023-02-07 21:28:32 +0500

    mn 0.06

--- cryptography_/models.py
+++ cryptography_/models.py
@@ -4,4 +4,4 @@ from django.db import models
 # Create your models here.
 class CryptoFile(models.Model):
     encoded = models.BooleanField(blank=True)
-    file = models.FileField(upload_to='cryptography/')
+    file = models.FileField(upload_to='media/')
