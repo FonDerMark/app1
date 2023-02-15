e015151e7b0f3149979bca9c5c3661ef042856ed
Dmitry <68408042+FonDerMark@users.noreply.github.com>
2023-02-15 18:27:53 +0500

    mn 0.033

--- pet_project_1/settings.py
+++ pet_project_1/settings.py
@@ -133,7 +133,7 @@ if ON_SERVER:
             'NAME': env('DB_NAME'),
             'USER': env('DB_USER'),
             'PASSWORD': env('DB_PASSWORD'),
-            'HOST': env('DB_HOST'),
+            'HOST': 'localhost',
             'PORT': env('DB_PORT'),
         }
     }
@@ -142,8 +142,12 @@ else:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': env('DB_NAME'),
+            'USER': env('DB_USER'),
+            'PASSWORD': env('DB_PASSWORD'),
+            'HOST': env('DB_HOST'),
+            'PORT': env('DB_PORT'),
         }
     }
 
