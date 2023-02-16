d3f119c20ffdef7bfeec0e97fec02c7c75c1e499
Dmitry <68408042+FonDerMark@users.noreply.github.com>
2023-02-16 21:29:56 +0500

    mn 0.041

--- upload/views.py
+++ upload/views.py
@@ -9,9 +9,12 @@ APP_ROOT = os.path.join(MEDIA_ROOT, 'files')
 # Create your views here.
 def index(request):
     if request.method == 'GET':
-        context = {
-            'files': os.scandir(APP_ROOT),
-        }
+        try:
+            context = {
+                'files': os.scandir(APP_ROOT),
+            }
+        except:
+            pass
         return render(request, 'web/html/upload/upload.html', context)
     if request.method == 'POST':
         if not os.path.exists(APP_ROOT):
@@ -20,6 +23,7 @@ def index(request):
         with open(os.path.join(APP_ROOT, file.name), 'wb') as f:
             f.write(file.read())
         return redirect('upload')
+
 
 def remove(request, filename):
     os.remove(os.path.join(APP_ROOT, filename))
